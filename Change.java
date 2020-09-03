f5278fc67a718202df417c2939566add66aad819
Colin Skinner <magi-nerv@magi.local>
2020-09-03 13:48:10 -0700

    Add a baseline change file.

---
+++ Change.java
@@ -0,0 +1,24 @@
+
+public class Change {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	pulic static int NumberOfWaysToMakeChange(int cents) {
+		//assume cents is non-negative
+		if (cents == 0 || cents < 5) {
+			return 1;
+		}
+		
+		if (cents < 5) {
+			
+		}
+		
+		//constrain ourselves to subset of coins available
+		
+		
+
+	}//method
+	
+}s
